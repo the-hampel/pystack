42db1d5acb40e2dec1be9d3ce0d9530934db4afd
Alexander Hampel <ahampel@flatironinstitute.org>
2022-12-22 13:54:07 -0500

    improve debug script

--- scripts/solid_dmft_debug_restart_cthyb.py
+++ scripts/solid_dmft_debug_restart_cthyb.py
@@ -12,10 +12,19 @@ from triqs_cthyb.solver import Solver
 
 np.set_printoptions(precision=4, suppress=True, linewidth=1040)
 
-try:
-    h5_file = glob.glob(sys.argv[1])[0]
-except:
-    raise ValueError('you have to specify the h5 file to start the debug run')
+
+################
+### params #####
+rank = None   # specify rank to run if wanted, otherwise set to None
+shells = None # specify shells to run cthyb, set to None to run all
+h5_file = None
+################
+
+if not h5_file:
+    try:
+        h5_file = glob.glob(sys.argv[1])[0]
+    except:
+        raise ValueError('you have to specify the h5 file to start the debug run')
 
 solver_list = []
 param_list = []
@@ -31,8 +40,13 @@ if not mpi_size == mpi.size:
     mpi.report(f'\n!!!! WARNING: number of mpi ranks differs. Current {mpi.size}, run on h5 had {mpi_size}.\n Results will differ.')
 
 
-for i_shell in range(n_shells):
+# for i_shell in range(n_shells):
+for i_shell in shells:
     mpi.report(f'\n-----\n impurity {i_shell}/{n_shells}\n parameter list:')
     mpi.report(param_list[i_shell])
     param_list[i_shell].pop('perform_tail_fit', False)
+    if rank:
+        mpi.report(f'running with random seed init for rank {rank}')
+        assert mpi.size == 1, 'rank random seed init only works for serial run'
+        param_list[i_shell]['random_seed'] = int(34788 + 928374 * rank)
     solver_list[i_shell].solve(h_int=h_int[i_shell], **param_list[i_shell])
